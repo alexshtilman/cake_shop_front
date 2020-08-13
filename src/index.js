0cf0ad256eb836e828c765811c2e0fc1204c8c3d
Alex Shtilman <gungam@outlook.com>
2020-08-13 23:31:57 +0300

    update to template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "./App.jsx";
+import * as serviceWorker from "./serviceWorker";
+import combineReducers from "./store/reducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(combineReducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
