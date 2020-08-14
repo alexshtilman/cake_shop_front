fbfa46800098763ce9a8878de3ce046641f1f4be
Alex Shtilman <gungam@outlook.com>
2020-08-14 17:01:49 +0300

    removed merge conflicts

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,12 @@
-#Pull latest official node image
-FROM node:latest
-
-# Expose ports
-EXPOSE 3000
-
-# Set working directory
+FROM node:alpine AS builder
 WORKDIR /app
+COPY . .
+RUN npm install react-scripts -g --silent
+RUN yarn install
+RUN yarn run build
 
-# Add /app/node_modules/.bin to environment variables
-ENV PATH /app/node_modules/.bin:$PATH
-
-# Copy package files and install app dependencies
-COPY package.json /app/package.json
-COPY package-lock.json /app/package-lock.json
-RUN npm install
-RUN npm install react-scripts -g
-
-# Add React app to working directory
-ADD . /app
-
-# Start the React app
-CMD ["npm", "start"]
+FROM node:alpine
+RUN yarn global add serve
+WORKDIR /app
+COPY --from=builder /app/build .
+CMD ["serve", "-p", "80", "-s", "."]
--- src/App.js
+++ src/App.js
@@ -1,26 +1,220 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  Button,
+  Container,
+  Dropdown,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    dropdownMenuStyle: {
+      display: "none",
+    },
+  };
+
+  handleToggleDropdownMenu = () => {
+    let newState = Object.assign({}, this.state);
+    if (newState.dropdownMenuStyle.display === "none") {
+      newState.dropdownMenuStyle = { display: "flex" };
+    } else {
+      newState.dropdownMenuStyle = { display: "none" };
+    }
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Grid padded className="tablet computer only">
+          <Container>
+            <Menu borderless inverted size="huge">
+              <Menu.Item header as="a">
+                Project Name
+              </Menu.Item>
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
+              <Dropdown text="Dropdown" className="item">
+                <Dropdown.Menu>
+                  <Dropdown.Item as="a">Action</Dropdown.Item>
+                  <Dropdown.Item as="a">Another action</Dropdown.Item>
+                  <Dropdown.Item as="a">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Navbar header</Dropdown.Header>
+                  <Dropdown.Item as="a">Seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">One more seperated link</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu>
+          </Container>
+        </Grid>
+        <Grid className="mobile only">
+          <Menu inverted borderless size="huge" fixed="top">
+            <Menu.Item header as="a">
+              Project Name
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <Button
+                  icon
+                  basic
+                  inverted
+                  toggle
+                  onClick={this.handleToggleDropdownMenu}
+                >
+                  <Icon name="content" />
+                </Button>
+              </Menu.Item>
+            </Menu.Menu>
+            <Menu
+              vertical
+              borderless
+              inverted
+              fluid
+              style={this.state.dropdownMenuStyle}
+            >
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
+              <Dropdown text="Dropdown" className="item">
+                <Dropdown.Menu>
+                  <Dropdown.Item as="a">Action</Dropdown.Item>
+                  <Dropdown.Item as="a">Another action</Dropdown.Item>
+                  <Dropdown.Item as="a">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Navbar header</Dropdown.Header>
+                  <Dropdown.Item as="a">Seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">One more seperated link</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu>
+          </Menu>
+        </Grid>
+
+        <Container>
+          <Segment vertical>
+            <Grid container stackable textAlign="center" columns={3}>
+              <Grid.Column>
+                <Image centered circular size="small" src="/square-image.png" />
+                <Header as="h1">Heading</Header>
+                <p>
+                  Donec sed odio dui. Etiam porta sem malesuada magna mollis
+                  euismod. Nullam id dolor id nibh ultricies vehicula ut id
+                  elit. Morbi leo risus, porta ac consectetur ac, vestibulum at
+                  eros. Praesent commodo cursus magna.
+                </p>
+                <Button basic>View details &raquo;</Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Image centered circular size="small" src="/square-image.png" />
+                <Header as="h1">Heading</Header>
+                <p>
+                  Donec sed odio dui. Etiam porta sem malesuada magna mollis
+                  euismod. Nullam id dolor id nibh ultricies vehicula ut id
+                  elit. Morbi leo risus, porta ac consectetur ac, vestibulum at
+                  eros. Praesent commodo cursus magna.
+                </p>
+                <Button basic>View details &raquo;</Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Image centered circular size="small" src="/square-image.png" />
+                <Header as="h1">Heading</Header>
+                <p>
+                  Donec sed odio dui. Etiam porta sem malesuada magna mollis
+                  euismod. Nullam id dolor id nibh ultricies vehicula ut id
+                  elit. Morbi leo risus, porta ac consectetur ac, vestibulum at
+                  eros. Praesent commodo cursus magna.
+                </p>
+                <Button basic>View details &raquo;</Button>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Grid stackable>
+              <Grid.Column width={10}>
+                <Header as="h1">
+                  First featurette heading.{" "}
+                  <span className="sub">It'll blow your mind.</span>
+                </Header>
+                <p>
+                  Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
+                  id ligula porta felis euismod semper. Praesent commodo cursus
+                  magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+                  ac cursus commodo.
+                </p>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Image src="/square-image.png" />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Grid stackable>
+              <Grid.Column width={6}>
+                <Image src="/square-image.png" />
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <Header as="h1">
+                  Oh yeah, <span className="sub">it's that good.</span>
+                </Header>
+                <p>
+                  Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
+                  id ligula porta felis euismod semper. Praesent commodo cursus
+                  magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+                  ac cursus commodo.
+                </p>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Grid stackable>
+              <Grid.Column width={10}>
+                <Header as="h1">
+                  And lastly, <span className="sub">this one.</span>
+                </Header>
+                <p>
+                  Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
+                  id ligula porta felis euismod semper. Praesent commodo cursus
+                  magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+                  ac cursus commodo.
+                </p>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Image src="/square-image.png" />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <Segment vertical>
+            <Grid columns={2}>
+              <Grid.Column>
+                &copy; 2017 Company, Inc. · <a href="#root">Privacy</a> ·{" "}
+                <a href="#root">Terms</a>
+              </Grid.Column>
+              <Grid.Column textAlign="right">
+                <a href="#root">Back to top</a>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
