a63b83d481b7cd5f12e8d2bf5fa3ac285206954b
Alex Shtilman <gungam@outlook.com>
2020-08-14 14:47:16 +0300

    test update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 #Pull latest official node image
-FROM node:14.8.0-alpine3.10
+FROM node:latest
 
 # Expose ports
 EXPOSE 3000
@@ -12,12 +12,12 @@ ENV PATH /app/node_modules/.bin:$PATH
 
 # Copy package files and install app dependencies
 COPY package.json /app/package.json
-COPY yarn.lock /app/yarn.lock
-RUN yarn install
-#RUN npm install react-scripts@3.4.3 -g --silent
+COPY package-lock.json /app/package-lock.json
+RUN npm install
+RUN npm install react-scripts -g
 
 # Add React app to working directory
 ADD . /app
 
 # Start the React app
-CMD ["yarn", "start"]
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "semantic-ui-css/semantic.min.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import combineReducers from "./store/reducers";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-
-const store = createStore(combineReducers);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
